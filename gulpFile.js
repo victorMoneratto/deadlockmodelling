7799624ab5045b2678accd1423d4a8d1cd3c7699
Victor Moneratto <victor.moneratto@gmail.com>
2015-10-13 22:40:51 -0300

    Project scaffold

---
+++ gulpFile.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var gulp       = require('gulp'),
+
+    jshint     = require('gulp-jshint'),
+    connect    = require('gulp-connect'),
+    uglify     = require('gulp-uglify'),
+    sass       = require('gulp-ruby-sass'),
+    prefix     = require('gulp-autoprefixer'),
+    jade       = require('gulp-jade'),
+    gulpif     = require('gulp-if'),
+    stylish    = require('jshint-stylish'),
+    del        = require('del'),
+    filter     = require('gulp-filter'),
+    sequence   = require('gulp-sequence'),
+    browserify = require('browserify'),
+    debowerify = require('debowerify'),
+    gutil      = require('gulp-util'),
+    source     = require('vinyl-source-stream'),
+    buffer     = require('vinyl-buffer'),
+    plumber    = require('gulp-plumber'),
+    combiner   = require('stream-combiner2');
+
+
+//Probably will use these in the future
+//var sourcemaps = require('gulp-sourcemaps'),
+//    imagemin   = require('gulp-imagemin'),
+//    csslint    = require('gulp-csslint'),
+//    jshint     = require('gulp-jshint'),
+//    pngcrush   = require('imagemin-pngcrush');
+
+
+var config = {
+    src: 'src',
+    env: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    lib: 'lib/',
+    port: 1881
+};
+config.build = 'build/' + config.env;
+
+gulp.task('jade', function () {
+    return gulp.src(config.src + '/**/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest(config.build))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+    //jshint
+    //gulp.src('/scripts/**/*.js')
+    //    ;
+
+    var b = browserify({entries: config.src + '/assets/scripts/app.js', debug: true});
+    b.transform(debowerify);
+    return b.bundle()
+        .on('error', function (err) {
+            gutil.log(err.message);
+            this.emit('end');
+        })
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest(config.build + '/assets/scripts/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('sass', function () {
+    var taskConfig = {};
+    if (config.env === 'production') {
+        taskConfig.sass = {style: 'compressed'};
+    } else {
+        taskConfig.sass = {};
+    }
+
+    return gulp.src(config.src + '/assets/styles/app.sass')
+        .pipe(sass())
+        .pipe(prefix('last 2 version'))
+        .pipe(gulp.dest(config.build + '/assets/styles/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('assets', function () {
+    return gulp.src([
+            config.src + '/assets/**/*',
+            '!' + config.src + '/assets/styles/*',
+            '!' + config.src + '/assets/scripts/*'])
+        .pipe(gulp.dest(config.build + '/assets'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+    gulp.watch([config.src + 'assets/scripts/**/*.js'], ['js']);
+    gulp.watch([config.src + '/**/*.jade'], ['jade']);
+    gulp.watch([config.src + '/assets/styles/**/*.sass'], ['sass']);
+});
+
+gulp.task('connect', function () {
+    connect.server({
+        root: config.build,
+        port: config.port,
+        livereload: true
+    });
+});
+
+gulp.task('clean', function () {
+    return del(config.build);
+});
+
+gulp.task('build', ['js', 'jade', 'sass', 'assets']);
+gulp.task('default', sequence('build', ['connect', 'watch']));
